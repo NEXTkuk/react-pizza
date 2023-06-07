d96ee2bb2fd8d0b61504925697c5a801dd246fcf
Anton <nextkuk@bk.ru>
2023-06-07 11:37:59 +0300

    Temp commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import axios from 'axios';
 import qs from 'qs';
 import { useNavigate } from 'react-router-dom';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
+import { setItems } from '../redux/slices/pizzasSlice';
 
 import Categories from '../components/Categories';
 import Sort, { list } from '../components/Sort';
@@ -19,11 +21,12 @@ const Home = () => {
   const isSearch = React.useRef(false);
   const isMounted = React.useRef(false);
 
+  const items = useSelector((state) => state.pizza.items);
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
+
   const [isReverseSort, setIsReverseSort] = React.useState(false);
 
   const { searchValue } = React.useContext(SearchContext);
-  const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   const onChangePage = (number) => {
@@ -34,62 +37,79 @@ const Home = () => {
     dispatch(setCategoryId(id));
   };
 
-  const fetchPizzas = () => {
+  const fetchPizzas = async () => {
     setIsLoading(true);
 
+    // NEW FETCH
+    const search = searchValue ? `&title_like=${searchValue}` : '';
+    const category = categoryId > 0 ? `&category=${categoryId}` : '';
+    const sortBy = `&_sort=${sort.sortProperty}`;
+    const order = isReverseSort ? '&_order=desc' : '&_order=asc';
+
+    await axios
+      .get(
+        `https://my-json-server.typicode.com/NEXTkuk/json-server/pizzas?_page=${currentPage}&_limit=4${category}${sortBy}${order}${search}`
+      )
+      .then((res) => {
+        console.log(res);
+        dispatch(setItems(res.data));
+        setIsLoading(false);
+      });
+
     // Костыль под осуждением
-    const getPage = () => {
-      if (currentPage === 1) {
-        return pizzas.slice(0, 4);
-      }
-      if (currentPage === 2) {
-        return pizzas.slice(4, 8);
-      }
-      if (currentPage === 3) {
-        return pizzas.slice(8, 10);
-      }
-      return pizzas;
-    };
+    // const getPage = () => {
+    //   if (currentPage === 1) {
+    //     return pizzas.slice(0, 4);
+    //   }
+    //   if (currentPage === 2) {
+    //     return pizzas.slice(4, 8);
+    //   }
+    //   if (currentPage === 3) {
+    //     return pizzas.slice(8, 10);
+    //   }
+    //   return pizzas;
+    // };
 
-    const pizzaPage = getPage();
+    // const pizzaPage = getPage();
 
-    const filteredItems = pizzaPage.filter((item) => (categoryId > 0 ? item.category === categoryId : item));
+    // const filteredItems = pizzaPage.filter((item) => (categoryId > 0 ? item.category === categoryId : item));
 
-    filteredItems.sort(function (a, b) {
-      // По популярности
-      if (sort.sortProperty === 'rating') {
-        if (isReverseSort) {
-          return b.rating - a.rating;
-        }
-        return a.rating - b.rating;
-      }
+    // filteredItems.sort(function (a, b) {
+    //   // По популярности
+    //   if (sort.sortProperty === 'rating') {
+    //     if (isReverseSort) {
+    //       return b.rating - a.rating;
+    //     }
+    //     return a.rating - b.rating;
+    //   }
 
-      // По цене
-      if (sort.sortProperty === 'price') {
-        if (isReverseSort) {
-          return b.price - a.price;
-        }
-        return a.price - b.price;
-      }
+    //   // По цене
+    //   if (sort.sortProperty === 'price') {
+    //     if (isReverseSort) {
+    //       return b.price - a.price;
+    //     }
+    //     return a.price - b.price;
+    //   }
 
-      // По Алфавиту
-      if (sort.sortProperty === 'title') {
-        if (isReverseSort) {
-          return b.title - a.title;
-        }
-        return a.title - b.title;
-      }
-      return 0;
-    });
+    //   // По Алфавиту
+    //   if (sort.sortProperty === 'title') {
+    //     if (isReverseSort) {
+    //       return b.title - a.title;
+    //     }
+    //     return a.title - b.title;
+    //   }
+    //   return 0;
+    // });
 
     // Замена fetch API
     // Фикс поиска
-    if (searchValue) {
-      setItems(pizzas);
-    } else {
-      setItems(filteredItems);
-    }
-    setIsLoading(false);
+    // if (searchValue) {
+    //   dispatch(setItems(pizzas));
+    //   // setItems(pizzas);
+    // } else {
+    //   // setItems(filteredItems);
+    //   dispatch(setItems(filteredItems));
+    // }
   };
 
   // Если первого рендер был и параметры изменились, то вшиваем URL
@@ -100,7 +120,6 @@ const Home = () => {
         categoryId,
         currentPage,
       });
-
       navigate(`?${queryString}`);
     }
     isMounted.current = true;
@@ -127,9 +146,12 @@ const Home = () => {
   React.useEffect(() => {
     window.scrollTo(0, 0);
 
-    if (isSearch.current) {
+    if (!isSearch.current) {
+      fetchPizzas();
+    } else {
       fetchPizzas();
     }
+    // fetchPizzas();
 
     isSearch.current = false;
   }, [categoryId, sort.sortProperty, isReverseSort, currentPage, searchValue]);
@@ -137,6 +159,7 @@ const Home = () => {
   const pizzasItems = items
     .filter((obj) => obj.title.toLowerCase().includes(searchValue.toLowerCase()))
     .map((obj) => (isLoading ? <LoadingBlock /> : <PizzaBlock key={obj.id} {...obj} />));
+
   const skeletons = [...new Array(6)].map((_, index) => <LoadingBlock key={index} />);
 
   return (
