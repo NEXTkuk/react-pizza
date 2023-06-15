bbccc7db073fbadbd961e98254a2c46e5ee29e7d
Anton <nextkuk@bk.ru>
2023-06-15 12:41:59 +0300

    Fix LocalStorage

--- src/utils/getCartFromLS.ts
+++ src/utils/getCartFromLS.ts
@@ -1,6 +1,18 @@
 import { calcTotalPrice } from './calcTotalPrice';
 
 export const getCartFromLS = () => {
+  try {
+    const currentDomain: string = window.location.pathname;
+    const LsDomain = localStorage.getItem('domain');
+
+    if (currentDomain !== LsDomain) {
+      localStorage.clear();
+      localStorage.setItem('domain', currentDomain);
+    }
+  } catch {
+    console.log('Ошибка с LS');
+  }
+
   const data = localStorage.getItem('cart');
   const items = data ? JSON.parse(data) : [];
   const totalPrice = calcTotalPrice(items);
